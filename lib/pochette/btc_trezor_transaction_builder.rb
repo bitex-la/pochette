3e0f5fb23d2bc8bde04dd29edfd0230e507f7d5d
werner <werner@yahoo.es>
2019-03-07 16:43:34 -0300

    Add SPENDWITNESS as a script_type when bech32

--- lib/pochette/btc_trezor_transaction_builder.rb
+++ lib/pochette/btc_trezor_transaction_builder.rb
@@ -14,10 +14,11 @@ class Pochette::BtcTrezorTransactionBuilder < Pochette::BaseTrezorTransactionBui
 
   def build_trezor_inputs
     base_build_trezor_inputs(inputs || []) do |input, hash, address|
-      if address.first.first.in?(%w[2 3])
-        hash[:amount] = input[3].to_s
-        hash[:script_type] = 'SPENDP2SHWITNESS'
-      end
+      p2sh_segwit_address = address.first.first.in?(%w[2 3])
+      native_segwit_address = address.first[0..2].in?(%w[bc1 tb1])
+      hash[:script_type] = 'SPENDP2SHWITNESS' if p2sh_segwit_address
+      hash[:script_type] = 'SPENDWITNESS' if native_segwit_address
+      hash[:amount] = input[3].to_s if p2sh_segwit_address || native_segwit_address
     end
   end
 end
